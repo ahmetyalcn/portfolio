c2fbc29b7cb69462894d6d223e51781e44634737
Ahmet Necdet Yalçın <ahmetnecdetyalcin@Ahmet-MacBook-Air.local>
2023-07-18 06:24:27 +0300

    güncellendi

--- src/sections/Skills.js
+++ src/sections/Skills.js
@@ -29,16 +29,16 @@ const Skills = () => {
         }
     ]
     return (
-        <div className="dark:bg-brown lg:h-[70vh]">
-            <div className="mx-auto rounded-xl overflow-hidden lg:w-[70%] pt-6 flex flex-wrap justify-between ">
-                <div className="lg:w-[15%]">
+        <div className="dark:bg-brown lg:h-[70vh] ">
+            <div className="mx-auto rounded-xl overflow-hidden lg:w-[70%] pt-6 flex flex-wrap w-[70%] ">
+                <div className="lg:w-[20%]">
 
-                    <h4 className="text-5xl p-5 mx-auto font-bold text-purple dark:text-green">{t("skills")}</h4>
+                    <h4 className="text-5xl pb-5 mx-auto font-bold text-purple dark:text-green">{t("skills")}</h4>
                 </div>
                 <div className="md:flex flex flex-wrap justify-end lg:w-[80%]">
                    
                           {skills.map((skill, index) => (
-                        <div key={index} className="flex items-center lg:justify-end w-[48%] flex-wrap pb-5">
+                        <div key={index} className="flex items-center justify-start lg:justify-end w-[50%] flex-wrap pb-5">
                             <img className="object-cover w-32 text-left" src={skill.imageUrl} alt="Modern building architecture" />
                             <p className=" text-xl p-1 w-32 text-left dark:text-white">{skill.title}</p>
                         </div>
